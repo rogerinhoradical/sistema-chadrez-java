63b7de5a547a594d99ef2211c7c4846ec6d7a1e4
Luiz Augusto <derpthin@gmail.com>
2020-03-29 20:58:28 -0300

    Comecando a implementar Tabuleiro e Peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,11 @@
 package application;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
+		Tabuleiro tab = new Tabuleiro(8, 8);
 	}
 
 }
